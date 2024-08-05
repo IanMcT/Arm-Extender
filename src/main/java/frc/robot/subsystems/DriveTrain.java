ee3855adbc130bb928b6054d3f1087d9168e515f
Ian McTavish <ian.mctavish@tldsb.on.ca>
2024-08-05 10:24:53 -0400

    DriveTrain Subsystem

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,14 +4,52 @@
 
 package frc.robot.subsystems;
 
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
+  private CANSparkMax DriveLeft;
+  private CANSparkMax DriveRight;
+  private DifferentialDrive driveTrain;
+
+  double speed, rotation;
+
   /** Creates a new DriveTrain. */
-  public DriveTrain() {}
+  public DriveTrain() {
+    DriveLeft = new CANSparkMax(10, MotorType.kBrushless);
+    DriveRight = new CANSparkMax(11, MotorType.kBrushless);
+
+   
+
+    DriveLeft.restoreFactoryDefaults();
+    DriveRight.restoreFactoryDefaults();
+
+    DriveLeft.setSmartCurrentLimit(30);
+    DriveRight.setSmartCurrentLimit(30);
+
+    driveTrain = new DifferentialDrive(DriveLeft, DriveRight);
+    
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    driveTrain.arcadeDrive(this.speed, this.rotation);
   }
+
+  public void drive(double s, double r){
+    this.speed = s;
+    this.rotation = r;
+  }
+
+  public void stop(){
+    this.speed = 0;
+    this.rotation = 0;
+  }
+
 }
